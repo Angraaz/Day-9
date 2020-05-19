ca5fbc048700d0c426b7e3ceb4b8ddf0f560ae9f
Angraaz <mvdsingh30@gmail.com>
2020-05-19 13:52:04 +0530

    Add Part Time Employee and Wage

---
+++ Case3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+echo "Welcome msg"
+
+empCheck=$((RANDOM % 3))
+isPartTime=1
+isFullTime=2
+salaryPerHr=20
+
+if [ $isPartTime -eq $empCheck ]
+then
+	echo "Present for part time"
+	workHrs=4
+elif [ $isFullTime -eq $empCheck ]
+then
+	echo "Present for Full Time"
+	workHrs=8
+else
+	echo "Absent"
+fi
+
+salary=$((workHrs * salaryPerHr))
+echo "Salary: "$salary
