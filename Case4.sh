dc0a16694385f8cf25d44795ad4dced30bb375c5
Angraaz <mvdsingh30@gmail.com>
2020-05-19 13:54:43 +0530

    Solve using Switch Case Statement

---
+++ Case4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+echo "Welcome msg"
+
+presenty=$((RANDOM % 3))
+isPartTime=1
+isFullTime=2
+salaryPerHr=20
+
+case $presenty in
+	$isPartTime)
+		echo "Present for part time"
+		workHrs=4
+		;;
+	$isFullTime)
+		echo "Present for Full Time"
+		workHrs=8
+		;;
+	*)
+		echo "Absent"
+		;;
+esac
+
+salary=$((workHrs * salaryPerHr))
+echo "Salary: "$salary
