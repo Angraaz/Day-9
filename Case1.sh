366d3cc404aaa15032802f6097e3821428b024dc
Angraaz <mvdsingh30@gmail.com>
2020-05-19 13:43:53 +0530

    Employee is Present or Absent

---
+++ Case1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+echo "Welcome msg"
+
+randomcheck=$((RANDOM%2));
+ispresent=1;
+
+if [ $ispresent -eq $randomcheck ]
+then
+      echo "Employee is present..";
+else
+      echo "Employee is Absent..";
+fi
