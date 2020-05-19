46b0b5e88a037551fbfaac09df641112d4423867
Angraaz <mvdsingh30@gmail.com>
2020-05-19 14:03:46 +0530

    Calculate wages till condition is Reached

---
+++ Case6.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+echo "Welcome msg"
+
+presenty=$((RANDOM % 3))
+isPartTime=1
+isFullTime=2
+salaryPerHr=20
+totalSalary=0
+workDays=20
+totalHrs=0
+days=0
+
+while [ $totalHrs -le 100 -a $days -le 20 ]
+do
+	presenty=$((RANDOM % 3))
+	case $presenty in
+		$isPartTime)
+			echo "Present for part time"
+			workHrs=4
+			$((days++))
+			;;
+		$isFullTime)
+			echo "Present for Full Time"
+			workHrs=8
+			$((days++))
+			;;
+		*)
+			echo "Absent"
+			;;
+	esac
+	totalHrs=$(($totalHrs + $workHrs))
+	salary=$((workHrs * salaryPerHr))
+	totalSalary=$(($totalSalary + $salary))
+done
+echo "Total Hours: "$totalHrs
+echo "Total Salary: "$totalSalary
